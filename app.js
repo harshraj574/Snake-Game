1ac19abe08d93910697a1044b45df4ddf48f8c40
harshraj574 <harshraj574@gmail.com>
2024-08-29 17:47:56 +0530

    version 2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     const arenaSize = 600;
     const cellSize = 20;
     let score = 0;
+    // let highScore = 0;
     let gameStarted = false;
     let food = {x: 300 , y: 200}; // {x: 15*20, y:10*20} -> cell cordinates-> pixels
     let snake = [{x:160,y:200}, {x:140,y:200}, {x:120,y:200}];
@@ -12,9 +13,10 @@ document.addEventListener("DOMContentLoaded",()=>{
     let dx= cellSize;
     let dy = 0;
 
-    // function newFood(x,y){
-    //     food = {x: Math.random()*(500),y: Math.random()*(500)};
-    // }
+    function newFood(){
+        food = {x: Math.floor(Math.random()*(20))*20,y: Math.floor(Math.random()*(20))*20};
+        // food = {x: Math.floor(Math.random()*30)*20,y: Math.floor(Math.random()*30)*20};
+    }
 
     function updateSnake(){
         const newHead = {x: snake[0].x+ dx, y: snake[0].y +dy};
@@ -23,8 +25,14 @@ document.addEventListener("DOMContentLoaded",()=>{
         //check collison wth food
         if(newHead.x === food.x && newHead.y === food.y){
             score += 10;
+            if(score > localStorage.getItem("highScore") ){
+                // highScore = score;
+                localStorage.setItem("highScore", score);
+            }
             document.getElementById('score-board').textContent = `Score: ${score}`;
-            // newFood(food.x,food.y);
+            document.getElementById('high-score').textContent = `HighScore: ${localStorage.getItem("highScore")}`;
+
+            newFood();
         }
         else{
             snake.pop(); // Remove tail
@@ -75,20 +83,22 @@ document.addEventListener("DOMContentLoaded",()=>{
         gameArena.appendChild(foodElement);
     }
 
+    function gameOver(){ 
+        if(snake[0].x > 560 || snake[0].y > (arenaSize-(snake.length*cellSize)-40)){
+            clearInterval(setId); 
+            // gameArena.innerHTML = '';
+        }
+        else if(snake[0].y < 20 || snake[0].x < 20 ){
+            clearInterval(setId);     
+            // gameArena.innerHTML = '';
+            // initiateGame();
+        }
+    }
+
     function gameLoop(){
        
         let setId =  setInterval(()=>{
-            // if(snake[0].x > 560 || snake[0].y > 560){
-            //     clearInterval(setId);
-            //     alert("game over");
-            //     gameArena.innerHTML = '';
-            // }
-            // else if(snake[0].y < 40 || snake[0].x < 40 ){
-            //     clearInterval(setId);
-            //     alert("game over");
-            //     gameArena.innerHTML = '';
-            //     // initiateGame();
-            // }
+            gameOver();
             updateSnake();
             drawFoodAndSnake();
         },200);   
@@ -107,6 +117,12 @@ document.addEventListener("DOMContentLoaded",()=>{
         const scoreBoard = document.createElement('div');
         scoreBoard.id = 'score-board';
 
+        const highScoreBoard = document.createElement('div');
+        highScoreBoard.id = 'high-score';
+
+        document.body.insertBefore(highScoreBoard,gameArena);
+        
+
         document.body.insertBefore(scoreBoard,gameArena); // insert scoreboard before gameArena div
 
         const startButton = document.createElement('button');
@@ -121,6 +137,7 @@ document.addEventListener("DOMContentLoaded",()=>{
         })
 
         document.body.appendChild(startButton);
+        // document.body.appendChild(highScoreBoard);
     }
 
     initiateGame();
